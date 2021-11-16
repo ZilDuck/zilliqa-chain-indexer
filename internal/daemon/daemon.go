58be32a85a3edd93ae61ce22eb49b32cf5bf84c4
Dan Tudor <dtudor01@gmail.com>
2021-11-16 20:09:18 +0000

    reset start to 0

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -48,7 +48,7 @@ func (d *Daemon) Execute() {
 func (d *Daemon) rewind() uint64 {
 	bestBlockNum, err := d.txRepo.GetBestBlockNum()
 	if err != nil {
-		return 943800
+		return 0
 	}
 
 	target := targetHeight(bestBlockNum)
