bfe9e542c195bf75943cca12c231a0492670aa2a
Dan Tudor <dtudor01@gmail.com>
2021-11-27 20:32:06 +0000

    Logger args

--- internal/elastic_cache/logger.go
+++ internal/elastic_cache/logger.go
@@ -9,5 +9,5 @@ type ElasticLogger struct {
 }
 
 func (l ElasticLogger) Printf(format string, v ...interface{}) {
-	zap.L().Error(fmt.Sprintf(format, v))
+	zap.L().Error(fmt.Sprintf(format, v...))
 }
