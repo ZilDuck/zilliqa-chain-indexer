7fef68dc8ee50e833d7e0499b88a9a97d8deb46e
dantudor <dtudor01@gmail.com>
2022-02-25 14:26:00 +0000

    Bug fix pulling assets

--- internal/metadata/metadata.go
+++ internal/metadata/metadata.go
@@ -74,12 +74,14 @@ func (s service) FetchImage(nft entity.Nft) error {
 		zap.L().With(zap.String("contract", nft.Contract), zap.Uint64("tokenId", nft.TokenId)).Error(err.Error())
 	}
 
-	zap.S().Debugf("Create asset folder for contract %s (if not exists)", nft.Contract)
 	contractDir := fmt.Sprintf("%s/%s", s.assetPath, nft.Contract)
-	_ = os.MkdirAll(contractDir, os.ModePerm)
+	zap.S().Debugf("Create asset folder for contract (if not exists): %s", contractDir)
+	if err = os.MkdirAll(contractDir, os.ModePerm); err != nil {
+		zap.L().With(zap.Error(err)).Error("Failed to create contract dir")
+	}
 
 	assetPath := fmt.Sprintf("%s/%d", contractDir, nft.TokenId)
-	zap.S().Debug("Using asset path: %s", assetPath)
+	zap.S().Debugf("Using asset path: %s", assetPath)
 	if _, err := os.Stat(assetPath); err == nil {
 		return ErrorAssetAlreadyExists
 	}
